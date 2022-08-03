b5918a6a40bcf790014569179a5a849ddfefb621
AlbertoAlonsoMc <80107902+AlbertoAlonsoMc@users.noreply.github.com>
2022-08-03 13:24:34 +0200

    Add files via upload

---
+++ Ejercicio5/Ej5.3/src/main/java/com/example/demo/PersonaService.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public interface PersonaService {
+
+    String getNombre();
+
+    String getPoblacion();
+
+    int getEdad();
+
+    public void setNombre(String nombre);
+
+    public void setPoblacion(String poblacion);
+
+    public void setEdad(int edad);
+}
