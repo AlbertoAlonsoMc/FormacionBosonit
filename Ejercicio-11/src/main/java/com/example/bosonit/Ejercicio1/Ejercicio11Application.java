678d36ffae94b8223dbbaea0ad4b9ca00201a522
AlbertoAlonsoMc <80107902+AlbertoAlonsoMc@users.noreply.github.com>
2022-08-11 18:03:52 +0200

    CRUD ESTRUCTURA HEXAGONAL

---
+++ Ejercicio-11/src/main/java/com/example/bosonit/Ejercicio1/Ejercicio11Application.java
@@ -0,0 +1,12 @@
+package com.example.bosonit.Ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio11Application.class, args);
+	}
+}
