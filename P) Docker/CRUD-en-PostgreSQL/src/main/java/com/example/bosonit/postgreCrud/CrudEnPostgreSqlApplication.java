bc779cca57a70f29980934a94a2184d14af13b14
BOSONIT\alberto.alonso <alberto.alonso@bosonit.com>
2022-09-06 14:52:39 +0200

    Proyecto Docker

---
+++ P) Docker/CRUD-en-PostgreSQL/src/main/java/com/example/bosonit/postgreCrud/CrudEnPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.bosonit.postgreCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEnPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEnPostgreSqlApplication.class, args);
+	}
+
+}
