69ea4f639b3993476379ca914449b8cf36490656
BOSONIT\alberto.alonso <alberto.alonso@bosonit.com>
2022-09-14 10:54:23 +0200

    Se crea DBA1

---
+++ R) DBA1. Querys Avanzadas/src/main/java/com/example/bosonit/Ejercicio1/DBA1.java
@@ -0,0 +1,12 @@
+package com.example.bosonit.Ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBA1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBA1.class, args);
+	}
+}
---
+++ R) DBA1. Querys Avanzadas/src/main/java/com/example/bosonit/Ejercicio1/infraestructure/PersonaInputDTORecord.java
@@ -0,0 +1,41 @@
+package com.example.bosonit.Ejercicio1.infraestructure;
+
+import com.example.bosonit.Ejercicio1.domain.Persona;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public record PersonaInputDTORecord(String usuario, String password, String name, String company_email,
+                                    String personal_email, String city, String surname, boolean active,
+                                    Date created_date, String imagen_url, Date termination_date) {
+
+    public Persona toPersona() throws Exception {
+        if (validarCampos()) {
+            return new Persona(usuario, password, name, company_email, personal_email, city, surname, active, created_date, imagen_url, termination_date);
+        } else {
+            return null;
+        }
+    }
+
+    private boolean validarCampos() throws Exception {
+        // Patrón para validar el email
+        Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+        Matcher matcher1 = pattern.matcher(company_email);
+        Matcher matcher2 = pattern.matcher(personal_email());
+
+        //Validando longitud de usuario
+        if (usuario.length() < 6 || usuario.length() > 10) {
+            throw new Exception("El usuario no puede tener menos de 6 caracteres ni más de 10");
+
+        } else if (!(matcher1.find() && matcher2.find())) {
+            throw new Exception("Patrón de email inválido");
+        }
+        return true;
+    }
+}
